80f84bfbcd49e9968ec72708abd75010292231e5
Victor Luiz <victorluiz19@yahoo.com.br>
2021-09-12 21:17:46 -0300

    commit icvip

---
+++ index.js
@@ -0,0 +1,9 @@
+// pagina inicial//
+
+var mensagem = "Bem Vindo ao Site Ficiticio da agência de Intercambio ";
+alert("Bem Vindo ao Site Ficiticio da agência de Intercambio ");
+
+var lista = 
+
+alert(lista [0]);
+
